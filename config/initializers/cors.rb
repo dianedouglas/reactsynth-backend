5b4466530a2200d1ac3f3665af7420bce334cc9d
Diane Douglas <diane.douglas1@gmail.com>
2025-06-29 18:16:20 -0400

    Update cors address.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "dianedouglas.github.io/reactsynth"
+    origins "localhost:5173", "https://dianedouglas.github.io"
 
     resource "*",
       headers: :any,
